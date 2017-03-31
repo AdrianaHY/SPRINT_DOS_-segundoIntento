2569495f7600bf430fabf3a31afaecff028613fc
AdrianaHY <adriana.herya@gmail.com>
2017-03-31 10:42:24 -0600

    Segundo intento ya con la lista y el li creado

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,39 +1,35 @@
 var contenedorSecciones= document.getElementById("contenedorListas");
 var contenedor = document.createElement("section");
 var inputNuevo = document.createElement("input");
+var lista = document.createElement("ul");
+//función para crear la lista, en donde mandaré llamar a las funciones de crear botón
+//y cerar nuevo input
 
 function crearLista(){
-  // var contenedorSecciones= document.getElementById("contenedorListas");
-  // var contenedor = document.createElement("section");
+
   contenedor.id=Date.now();
   var nombreLista=document.getElementById("nombreLista").value;
   contenedorSecciones.appendChild(contenedor);
   var titulo = document.createElement("h2");
   titulo.innerText= nombreLista;
   contenedor.appendChild(titulo);
-  // var inputNuevo = document.createElement("input");
-  // inputNuevo.id = Date.now();
-  // contenedor.appendChild(inputNuevo);
-  // var botonNuevo= document.createElement("input");
-  // botonNuevo.type = "button";
-  // botonNuevo.value = "Crear pendiente";
-  // botonNuevo.onclick=crearPendiente(inputNuevo.id);
-  // contenedor.appendChild(botonNuevo);
   crearNuevoInput();
-
+  // crearBoton();
 
   //console.log(contenedor.id);
 
 }
 
+//función para crear el nuevo input
 function crearNuevoInput(){
 
-  inputNuevo.id = Date.now();
+  inputNuevo.id = Datenow();
   contenedor.appendChild(inputNuevo);
-  console.log(inputNuevo.id);
+  console.log(inputNuevo);
   crearBoton();
 }
 
+//función para crear el nuevo botón, que después será llamada dentro del input
 function crearBoton(inputId){
   var botonNuevo= document.createElement("input");
   botonNuevo.type = "button";
@@ -44,11 +40,14 @@ function crearBoton(inputId){
 
 }
 
+//función para crear el pendiente
 function crearPendiente(){
   var nombrePendiente = this.getAttribute("id").value;
   var nuevoPendiente = document.createElement("li");
   nuevoPendiente.id = Date.now();
   var texto = document.createTextNode(inputNuevo.value);
-  contenedor.appendChild(texto);
+  nuevoPendiente.appendChild(texto);
+  lista.appendChild(nuevoPendiente);
+  contenedor.appendChild(lista);
   console.log(nombrePendiente);
 }
